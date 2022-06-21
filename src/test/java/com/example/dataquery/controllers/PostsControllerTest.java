427ae759328c36440afd3471d5c40b2db28943b0
pomindeoq <mindaugas.dirma@gmail.com>
2022-06-21 22:13:46 +0300

    tests and docker fix

--- src/test/java/com/example/dataquery/controllers/PostsControllerTest.java
+++ src/test/java/com/example/dataquery/controllers/PostsControllerTest.java
@@ -1,31 +1,13 @@
 package com.example.dataquery.controllers;
 
-import com.example.dataquery.DataQueryApplication;
 import com.example.dataquery.DataQueryApplicationTests;
 import com.example.dataquery.models.PostDTO;
 import com.example.dataquery.services.FilterService;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.util.ArrayList;
 import java.util.List;;
 
-@ExtendWith(MockitoExtension.class)
 class PostsControllerTest extends DataQueryApplicationTests {
 
     @Autowired
@@ -35,9 +17,29 @@ class PostsControllerTest extends DataQueryApplicationTests {
     private PostsController postsController;
 
     @Test
-    void searchPostsSingleEqualsTitleSecond_success() {
+    void searchPostsSingleEqualsIdSecond_success() {
         String query = "EQUAL(id,\"second-post\")";
         List<PostDTO> expectedPosts = List.of(setup_post2());
+        List<PostDTO> actualPosts = postsController.search(query).getBody();
+
+        assertEquals(1, actualPosts.size());
+        assertEquals(expectedPosts, actualPosts);
+    }
+
+    @Test
+    void searchPostsDoubleEqualsIdViewsFirst1_success() {
+        String query = "EQUAL(id,\"second-post\"),LESS_THAN(views,50)";
+        List<PostDTO> expectedPosts = List.of(setup_post2());
+        List<PostDTO> actualPosts = postsController.search(query).getBody();
+
+        assertEquals(1, actualPosts.size());
+        assertEquals(expectedPosts, actualPosts);
+    }
+
+    @Test
+    void searchPostsEqualsLessIdViewsSecond50_success() {
+        String query = "EQUAL(id,\"first-post\"),EQUAL(views,1)";
+        List<PostDTO> expectedPosts = List.of(setup_post1());
         List<PostDTO> actualPosts = postsController.search(query).getBody();
 
         assertEquals(1, actualPosts.size());
@@ -95,14 +97,36 @@ class PostsControllerTest extends DataQueryApplicationTests {
     }
 
     @Test
-    void searchPostsAndViewsId_success() {
-        String query = "GREATER_THAN(timestamp,1555832955)";
-        List<PostDTO> expectedPosts = List.of();
+    void searchPostsAndViewsContentScala50_success() {
+        String query = "AND(EQUAL(content,\"Scala Tutorial\"),EQUAL(views,50))";
+        List<PostDTO> expectedPosts = List.of(setup_post3());
         List<PostDTO> actualPosts = postsController.search(query).getBody();
 
-        assertEquals(0, actualPosts.size());
+        assertEquals(1, actualPosts.size());
         assertEquals(expectedPosts, actualPosts);
     }
+
+    @Test
+    void searchPostsORidFirstSecond_success() {
+        String query = "OR(EQUAL(id,\"first-post\"),EQUAL(id,\"second-post\"))";
+        List<PostDTO> expectedPosts = List.of(setup_post1(), setup_post2());
+        List<PostDTO> actualPosts = postsController.search(query).getBody();
+
+        assertEquals(2, actualPosts.size());
+        assertEquals(expectedPosts, actualPosts);
+    }
+
+    @Test
+    void searchPostsOR_views_1_OR_100_success() {
+        String query = "OR(EQUAL(views,1),EQUAL(views,100))";
+        List<PostDTO> expectedPosts = List.of(setup_post1());
+        List<PostDTO> actualPosts = postsController.search(query).getBody();
+
+        assertEquals(1, actualPosts.size());
+        assertEquals(expectedPosts, actualPosts);
+    }
+
+
     private PostDTO setup_post1() {
         return PostDTO.builder()
                 .id("first-post")
